99cd305524a8f1b673d35c74d38a7bbc444054c6
poine <poinix@gmail.com>
2018-11-12 17:10:47 +0100

    world to cam in demo z

--- scripts/find_world_to_cam.py
+++ scripts/find_world_to_cam.py
@@ -36,7 +36,7 @@ def load_points(path):
 
 
 def main(args):
-    if 1:
+    if 0:
         image_path =  '../test/F111/f111_cam_1_floor_01.png'
         camera_path = '../test/F111/ueye_enac_ceiling_1_6mm.yaml'
         points_path = '../test/F111/ueye_enac_ceiling_1_extrinsic_points.yaml'
@@ -48,7 +48,10 @@ def main(args):
         image_path =  '../test/enac_bench/floor.png'
         camera_path = '../params/enac_demo_bench/ueye_enac_ceiling_3.yaml'
         points_path = '../test/enac_bench/floor_extrinsic_points.yaml'
-
+    if 1:
+        image_path =  '../test/enac_demo_z/cam1.png'
+        camera_path = '../params/enac_demo_z/ueye_enac_z_1.yaml'
+        points_path = '../test/enac_demo_z/cam1_floor_extrinsic_points.yaml'
         
         
     img = cv2.imread(image_path)
